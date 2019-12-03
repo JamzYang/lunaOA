4017d02e85085a2347411580b16da9f8f7ab6386
JamzYang <yangshen727@163.com>
2019-12-03 23:50:01 +0800

    'yml'

---
+++ src/main/java/com/sjz/LunaApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yang
+ * Date 2019/12/3 23:42
+ */
+@SpringBootApplication
+public class LunaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LunaApplication.class,args);
+    }
+}
