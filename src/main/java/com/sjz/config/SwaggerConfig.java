44d7339d340e3d4636aeba8c878678af207a3881
JamzYang <yangshen727@163.com>
2019-12-08 23:32:38 +0800

    swagger 增加请求头

--- src/main/java/com/sjz/config/SwaggerConfig.java
+++ src/main/java/com/sjz/config/SwaggerConfig.java
@@ -13,14 +13,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -41,6 +45,12 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("token").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        tokenPar.build();
+        pars.add(tokenPar.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
             .apiInfo(apiInfo())
             .select()
@@ -50,7 +60,8 @@ public class SwaggerConfig implements WebMvcConfigurer {
             .apis(RequestHandlerSelectors.basePackage("com.sjz"))
             .paths(PathSelectors.any())
             .build()
-            .securitySchemes(security());
+            .securitySchemes(security())
+            .globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
